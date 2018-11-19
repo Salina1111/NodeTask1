7cabf9ff6cb7c5035fdca13f4946742f884c10d9
Salina <salina.sallu@outlook.com>
2018-11-19 12:53:12 +0545

    patch is modified

--- asyncAwait/asyncAwait.js
+++ asyncAwait/asyncAwait.js
@@ -144,8 +144,12 @@ exports.patch = async (req, res) => {
         }
         
         //Patch request for making the deleted boolean -> true
-        note.deleted = true;
-        
+        if (note.deleted == false) {
+            note.deleted = true;
+        } 
+        else {
+            note.deleted = false;
+        }
         
         //save it
         const save = await note.save();
